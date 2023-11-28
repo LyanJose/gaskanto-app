88a451a42381b731fa73ed5f872ed2c5095f9b16
LyanJosePayn <liamjose.rakotoarison452@gmail.com>
2023-11-28 10:16:24 +0300

    pwa - test 1

---
+++ app.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Votre code JavaScript ici
+    // Par exemple, enregistrez des données localement
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('service-worker.js')
+            .then((registration) => {
+                console.log('Service Worker enregistré avec succès:', registration);
+            })
+            .catch((error) => {
+                console.log('Erreur lors de l\'enregistrement du Service Worker:', error);
+            });
+    }
+});
